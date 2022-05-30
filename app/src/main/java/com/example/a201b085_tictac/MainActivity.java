c819ec064baa50cbef05750b94022f1f50c6879c
Chinmay9460 <balichwal02@gmail.com>
2022-05-30 19:42:09 +0530

    TicTacToe

---
+++ app/src/main/java/com/example/a201b085_tictac/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.a201b085_tictac;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+    }
+}
